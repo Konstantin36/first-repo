a34156c65fd9de465d4152038c37898f1c849904
Konstantin36 <koce123456789koce@gmail.com>
2016-10-22 16:37:46 +0300

    Add files via upload

---
+++ REVERSE String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    string inputString = Console.ReadLine();
+
+    string resultString=string.Join(" ",inputString
+    .Split()
+    .Select(x=>new String(x.Reverse().ToArray())));
+    Console.WriteLine(resultString);
+    }    
+}
+
